a8b60162c86dd71b64e6642ee9380c6973a1e5ad
Shivam Kumar <life.shivam2394@gmail.com>
2023-08-04 00:58:47 +0530

    Added All Routing Pages and their Paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
-import Layout from './components/Layout/Layout';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import About from './pages/About.js';
+import Contact from './pages/Contact.js';
+import Policy from './pages/Policy.js';
+import PageNotFound from './pages/PageNotFound.js';
 
 function App() {
   return (
     <>
-      <Layout>
-        <h1>E-Commerce Application</h1>
-      </Layout>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/policy' element={<Policy />} />
+        <Route path='/*' element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
