6add801c4d8631f4bd0d2c939894fb13cf4ca19c
Shivam Kumar <life.shivam2394@gmail.com>
2023-08-04 12:26:55 +0530

    Header Changes

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <NavLink to='/' className="navbar-brand" href="#">
+                        shhiivvaam
+                    </NavLink>
+                    <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
+                        <li className="nav-item active">
+                            <NavLink to='/' className="nav-link" href="#">Home {/* <span className="sr-only">(current)</span> */}</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to='/register' className="nav-link" href="#">Register</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to='/login' className="nav-link" href="#">Login</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to='/cart' className="nav-link" href="#">Cart(0)</NavLink>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+
+        </>
     )
 }
 
